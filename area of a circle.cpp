9a9c945f84a0209986d3295a66baa47422b05e45
Vandux1997 <113585996+Vandux1997@users.noreply.github.com>
2022-10-10 00:46:58 -0700

    Add files via upload
    my c++ program assignments for area of the circle and conversions from celcius to fahrenheit

---
+++ area of a circle.cpp
@@ -0,0 +1,13 @@
+//program to find the area of a circle
+#include<iostream>
+using namespace std;
+int main ()
+{
+	float celcius,fahrenheit;
+	cout<<"Enter the temperature in celcius:";
+	cin>>celcius;
+	fahrenheit=(celcius*5/9)+32;
+	cout<<"endlEquivalent temperature in fahrenheit:"<<fahrenheit;
+	cout<<endl;
+	return 0;
+}
